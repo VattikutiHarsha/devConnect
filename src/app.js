fb7c33cf196731fe5b3c93f9f80ca5d7757da6ef
VattikutiHarsha <v.harshavinay5@gmail.com>
2025-02-06 22:02:39 +0530

    Update & Validators

--- src/app.js
+++ src/app.js
@@ -9,8 +9,12 @@ app.post("/signup", async ( req, res) => {
     //Creating a new instance of User model
     const user = new User(req.body);
 
-    await user.save();
-    res.send("User added Successfully");
+    try{
+        await user.save();
+        res.send("User added Successfully");
+    } catch (err) {
+        res.status(400).send("Error saving the user:" + err.message);
+    }
 });
 
 //Get User from DB
@@ -48,14 +52,24 @@ app.delete("/user", async( req, res ) => {
     }
 });
 
-app.patch("/user", async( req, res ) => {
-    const userId = req.body.userId;
+app.patch("/user/:userId", async( req, res ) => {
+    const userId = req.params?.userId;
     const data = req.body;
     try{
-        const user = await User.findByIdAndUpdate({_id: userId}, data);
+        const ALLOWED_UPDATES = ["gender", "age", "skills"];
+        const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
+        if(!isUpdateAllowed) {
+            throw new Error("Update not allowed");
+        }
+        if(data?.skills.length > 10){
+            throw new Error("Skills can't be more than 10");
+        }
+        const user = await User.findByIdAndUpdate({_id: userId}, data, {
+            runValidators: true,
+        });
         res.send("User updated successfully");
     } catch (err) {
-        res.status(400).send("Something went wrong");
+        res.status(400).send("Update failed:" + err.message);
     }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,38 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
+        required: true,
     },
     lastName: {
         type: String,
     },
     emailId: {
         type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        validate(value){
+            if(!validator.isEmail(value)) {
+                throw new Error("Invalid email address: " + value);
+            }
+        },
     },
     password: {
         type: String,
+        required: true,
     },
     age: {
         type: Number,
     },
     gender: {
         type: String,
-    }
+    },
+    skills: {
+        type: [String],
+    },
 });
 
 module.exports = mongoose.model("User", userSchema);
